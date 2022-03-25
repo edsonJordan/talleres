debae1dfc3314e69a61713d6346d271871134e83
Edson Jordan <edson_4555@hotmail.com>
2022-03-25 00:03:30 -0500

    adding taller3

---
+++ taller3/README.md
@@ -0,0 +1,2 @@
+# HomeDesign
+Nuestro verdadero negocio no es vender propiedades, sino ayudar a nuestros clientes a tomar la decisión que más les convenga.
---
+++ taller3/script.js
@@ -0,0 +1,5 @@
+document.getElementById("amburger-icon").addEventListener("click", function() {
+    this.classList.toggle("active");
+    document.getElementById("navigation__list").classList.toggle("none");
+});
+
